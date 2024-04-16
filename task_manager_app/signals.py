9201eebcda6dafe8345d7f286e3f9e78e2a2f57b
TristanM <tristanmoffett36@gmail.com>
2024-04-16 06:46:30 -0600

    Commented out the signals and some settings information

--- task_manager_app/signals.py
+++ task_manager_app/signals.py
@@ -5,16 +5,16 @@ from django.utils import timezone
 from .models import Task
 from django.conf import settings
 
-@receiver(post_save, sender=Task)
-def taskOverdueNotification(sender, instance, created, **kwargs):
+#@receiver(post_save, sender=Task)
+#def taskOverdueNotification(sender, instance, created, **kwargs):
 
-    if (instance.completion_stage == 'Not Started' or instance.completion_stage == 'Working On') and instance.due_date < timezone.now().date():
-        # Get the team lead's email associated with the task's assignee
-        team_lead_email = instance.assignee.team.lead.user.email
+#    if (instance.completion_stage == 'Not Started' or instance.completion_stage == 'Working On') and instance.due_date < timezone.now().date():
+#        # Get the team lead's email associated with the task's assignee
+#        team_lead_email = instance.assignee.team.lead.user.email
 
         # Send email to team lead
-        subject = 'Task Overdue: {}'.format(instance.title)
-        message = 'The task "{}" assigned to {} is overdue'.format(instance.title, instance.assignee.name)
-        from_email = settings.EMAIL_HOST_USER
-        recipient_list = [team_lead_email]
-        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+#        subject = 'Task Overdue: {}'.format(instance.title)
+#        message = 'The task "{}" assigned to {} is overdue'.format(instance.title, instance.assignee.name)
+#        from_email = settings.EMAIL_HOST_USER
+#        recipient_list = [team_lead_email]
+#        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
