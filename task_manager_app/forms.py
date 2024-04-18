31172fdebdc62c74322f8fd500b8f584d6cc60d1
TristanM <tristanmoffett36@gmail.com>
2024-04-18 11:09:54 -0600

    Updated Front-End for multiple pages such as -> Home Page, Login Page, Register Page, Create Team Page, and Join Team Page

--- task_manager_app/forms.py
+++ task_manager_app/forms.py
@@ -3,6 +3,7 @@ from .models import *
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # What is a form forms?
 """ 
