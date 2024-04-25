b2c4dd4c3c2ba90b877e0ac283dbc6334a5f94d8
TristanM <tristanmoffett36@gmail.com>
2024-04-25 10:44:52 -0600

    Added comments

--- task_manager_app/test_views.py
+++ task_manager_app/test_views.py
@@ -10,6 +10,10 @@ class TestViews(TestCase):
         self.team_lead_group = Group.objects.create(name='Team Lead')
         self.team_member_group = Group.objects.create(name='Team Member')
 
+    ##########################################
+    #               Create Task              #
+    ##########################################
+
     def test_create_task(self):
         # Creates a team
         team = Team.objects.create(name='Test Team')
@@ -27,6 +31,10 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 302)  # Check if the view redirects after creating a task
         self.assertEqual(response.url, reverse('team-member-tasks', kwargs={'team_member_id': team_member.id}))  # Check if the redirection is to the correct URL
 
+    ##########################################
+    #               Create Team              #
+    ##########################################
+
     def test_create_team_page(self):
         self.client.force_login(self.user)
         response = self.client.post(reverse('create-team-page'), {'team_name': 'Test Team'})
@@ -34,6 +42,10 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 302)  # Check if the view redirects after creating a team
         self.assertEqual(response.url, reverse('index'))  # Check if the redirection is to the correct URL
         self.assertTrue(Team.objects.filter(name='Test Team').exists())  # Check if the team is created in the database
+
+    ##########################################
+    #                Join Team               #
+    ##########################################
 
     def test_join_team_page(self):
         team = Team.objects.create(name='Test Team')
